0f8afb8b5fab781ad035b5e89d921ac828fdb97a
Viljami <vruokonen@gmail.com>
2021-01-15 16:08:06 +0200

    added port constatn to index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express'),
-    app = express();
+    app = express(),
+    port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send('Mee Heroku app.');
 });
 
-app.listen(5000, () => {
+app.listen(port, () => {
     console.log('Server running in port 5000');
 });
 
